b688a8bf97f3c9481001b39dbf67abc0f166fdb4
Maria <mariafrances@student.ie.edu>
2022-06-16 14:18:38 +0200

    error

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
 from flask import Flask
-from flask import render_template, request, redirect
-from main import app, db
 
-import socket
-jedi = "of the jedi"
+from flask_sqlalchemy import SQLAlchemy
+
+from flask import render_template, request, redirect
+from flask_migrate import Migrate
+from sqlalchemy import create_engine
+#import mysql.connector
+
+app = Flask(__name__)
+PASSWORD ="secretpassword"
+PUBLIC_IP_ADDRESS ="35.205.41.62"
+DBNAME ="testing"
+PROJECT_ID ="team2-353417"
+INSTANCE_NAME ="team2-353417:europe-west1:databaseuser"
+SQLALCHEMY_DATABASE_URI = f'mysql+mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}'
+# configuration
+app.config["SECRET_KEY"] = "zwBhXws0knq7dZ6f0ciIK3fl5cefusJRtFEiU5Hi"
+app.config["SQLALCHEMY_DATABASE_URI"]= SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
+
+
+db = SQLAlchemy(app)
+class dates(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    tittle = db.Column(db.String(64), index=True, nullable=False)
+    description = db.Column(db.String(120), index=True, nullable=False)
+    status = db.Column(db.Boolean, default=False)
+    extend_existing=True
+
+    
+db.create_all()
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
+#connection = mysql.connector.connect(user='root',password='secretpassword', host='35.205.41.62',datase='testing')
+#connection=create_engine.connect()
+#cursor=connection.cursor()
+connection=engine.connect()
+
 
 @app.route('/')
-@app.route('/index', methods=['GET'])
+@app.route('/index')
 def index():
     # entries = [
     #     {
@@ -22,11 +56,16 @@ def index():
     #         'status': False
     #     }
     # ]
-    try:
-        entries = dates.query.all()
+    insert_brochure_query = """
+    SELECT * FROM dates
+    """
+    with engine.connect() as connection:
+        entries=connection.execute(insert_brochure_query)
         return render_template('index.html', entries=entries)
-    except socket.error:
-        return False
+
+    #entries = connection.query.all()
+    #return render_template('index.html', entries=entries)
+   
 
 @app.route('/add', methods=['POST'])
 def add():
@@ -93,3 +132,7 @@ def turn(id):
 @app.errorhandler(Exception)
 def error_page(e):
     return "error"
+
+if __name__ == '__main__':
+    app.run()
+#import app.routes
--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from app import db
+from main import db
 
 class dates(db.Model):
     
