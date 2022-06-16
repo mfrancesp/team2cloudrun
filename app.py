ab61c64751d27e65fb8800237b47447b8be4aa30
Maria <mariafrances@student.ie.edu>
2022-06-16 20:48:32 +0200

    Change

--- app.py
+++ app.py
@@ -1,157 +1,84 @@
 from flask import Flask
-
 from flask_sqlalchemy import SQLAlchemy
-
 from flask import render_template, request, redirect
 from flask_migrate import Migrate
 from sqlalchemy import create_engine
-
 import mysql.connector
 from mysql.connector import Error
+from sqlalchemy.engine.base import Connection
+
+
 app = Flask(__name__)
-PASSWORD ="secretpassword"
-PUBLIC_IP_ADDRESS ="35.205.41.62"
-DBNAME ="databaseuser"
-PROJECT_ID ="team2-353417"
-INSTANCE_NAME ="team2-353417:europe-west1:databaseuser"
-SQLALCHEMY_DATABASE_URI = f'mysql+mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}'
-# configuration
-app.config["SECRET_KEY"] = "zwBhXws0knq7dZ6f0ciIK3fl5cefusJRtFEiU5Hi"
-app.config["SQLALCHEMY_DATABASE_URI"]= SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
 
- 
+db_user = "user1"
+db_password = "1234"
+db_name = "testing"
+db_connection_name = "team2-353417:europe-west1:databaseuser"
+host = "35.205.41.62"
 
-db = SQLAlchemy(app)
-class dates(db.Model):
-    
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    tittle = db.Column(db.String(64), index=True, nullable=False)
-    description = db.Column(db.String(120), index=True, nullable=False)
-    status = db.Column(db.Boolean, default=False)
-    extend_existing=True
+engine_url = 'mysql+pymysql://{}:{}@{}/{}'.format(
+        db_user, db_password, host, db_name)
 
-    
-db.create_all()
-try:
-    connection = mysql.connector.connect(host=PUBLIC_IP_ADDRESS,
-                                         database='testing',
-                                         user='root',
-                                         password='secretpassword')
-    if connection.is_connected():
-        db_Info = connection.get_server_info()
-        print("Connected to MySQL Server version ", db_Info)
-        cursor = connection.cursor()
-        cursor.execute("select database();")
-        record = cursor.fetchone()
-        print("You're connected to database: ", record)
-        insert_brochure_query = """
-            SELECT * FROM dates
-            """
-    
-        entries=cursor.execute(insert_brochure_query)
-except Error as e:
-    print("Error while connecting to MySQL", e)
-
-#engine = create_engine(SQLALCHEMY_DATABASE_URI)
-#connection = mysql.connector.connect(user='root',password='secretpassword', host='35.205.41.62',datase='testing')
-#connection=create_engine.connect()
-#cursor=connection.cursor()
+engine = create_engine(engine_url)
 
 
 
 @app.route('/')
-@app.route('/index')
-def index():
-    # entries = [
-    #     {
-    #         'id' : 1,
-    #         'title': 'test title 1',
-    #         'description' : 'test desc 1',
-    #         'status' : True
-    #     },
-    #     {
-    #         'id': 2,
-    #         'title': 'test title 2',
-    #         'description': 'test desc 2',
-    #         'status': False
-    #     }
-    # ]
-    insert_brochure_query = """
-    SELECT * FROM dates
-    """
-    
-    #entries=connection.execute(insert_brochure_query)
-    return render_template('index.html', entries=entries)
-
-    #entries = connection.query.all()
-    #return render_template('index.html', entries=entries)
-   
+def start():
+    query="SELECT * FROM dates"
+    with engine.connect() as connection:
+        entries=connection.execute(query).fetchall()
+        
+        return render_template('index.html', entries=entries)
 
 @app.route('/add', methods=['POST'])
 def add():
     if request.method == 'POST':
-        form = request.form
-        title = form.get('title')
-        description = form.get('description')
-        if not title or description:
-            entry = dates(title = title, description = description)
-            db.session.add(entry)
-            db.session.commit()
+        form=request.form
+        title=str(form.get('title'))
+        description=str(form.get('description'))
+        
+        query_insert="INSERT INTO dates (tittle,description,status) VALUES ('%s','%s','1');" % (title,description)
+        with engine.connect() as connection:
+            connection.execute(query_insert)
             return redirect('/')
-
-    return "of the jedi"
-
-@app.route('/update/<int:id>')
-def updateRoute(id):
-    if not id or id != 0:
-        entry = dates.query.get(id)
-        if entry:
-            return render_template('update.html', entry=entry)
-
-    return "of the jedi"
-
-@app.route('/update/<int:id>', methods=['POST'])
-def update(id):
-    if not id or id != 0:
-        entry = dates.query.get(id)
-        if entry:
-            form = request.form
-            title = form.get('title')
-            description = form.get('description')
-            entry.title = title
-            entry.description = description
-            db.session.commit()
-        return redirect('/')
-
-    return "of the jedi"
-
-
 
 @app.route('/delete/<int:id>')
 def delete(id):
+    print(id)
     if not id or id != 0:
-        entry = dates.query.get(id)
-        if entry:
-            db.session.delete(entry)
-            db.session.commit()
-        return redirect('/')
-
-    return "of the jedi"
+        
+        query_delete="DELETE FROM dates WHERE id=%s " % (id)
+        with engine.connect() as connection:
+            connection.execute(query_delete)
+            return redirect('/')
 
 @app.route('/turn/<int:id>')
 def turn(id):
     if not id or id != 0:
-        entry = dates.query.get(id)
-        if entry:
-            entry.status = not entry.status
-            db.session.commit()
-        return redirect('/')
+        query_turn="UPDATE dates SET status='0' WHERE id=%s" %(id)
+        with engine.connect() as connection:
+            connection.execute(query_turn)
+            return redirect('/')
 
-    return "of the jedi"
-
-
+@app.route('/update/<int:id>', methods=['POST', 'GET'])
+def update(id):
+    if request.method == 'POST':
+        if not id or id != 0:
+            form=request.form
+            title=str(form.get('title'))
+            description=str(form.get('description'))
+            
+            query_insert="UPDATES dates SET tittle='%s', description='%s'WHERE id=%s;" % (title,description,id)
+            with engine.connect() as connection:
+                connection.execute(query_insert)
+                return redirect('/')
+    if request.method=='GET':
+        with engine.connect() as connection:
+                entry=connection.execute("SELECT * FROM dates WHERE id=%s" %(id))
+                return render_template('update.html', entry=entry)
+       
 
 if __name__ == '__main__':
-    app.run()
-#import app.routes
+    app.run(debug=True)
+
